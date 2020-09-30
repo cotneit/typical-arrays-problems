13865dfad492a6ca5f5778c4d6c4ddd8ce0b71ee
Vladyslav Karasov <36513243+cotneit@users.noreply.github.com>
2020-09-30 16:28:26 +0300

    feat: solve tasks

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array || array.length === 0) {
+    return 0;
+  }
+
+  const min = Math.min(...array);
+
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || array.length === 0) {
+    return 0;
+  }
+
+  const max = Math.max(...array);
+
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || array.length === 0) {
+    return 0;
+  }
+
+  const sum = array.reduce((acc, curr) => acc + curr)
+  const avg = sum / array.length
+
+  return avg;
 }
